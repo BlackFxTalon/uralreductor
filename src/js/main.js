0f3fbf7f3ddc4bbaa2b6260f9bd355ac655babe2
blackfxtalon <andyv@inbox.ru>
2022-05-25 02:12:06 +0500

    ready

--- src/js/main.js
+++ src/js/main.js
@@ -82,3 +82,63 @@ function toggleToTopBtn() {
 }
 
 toggleToTopBtn();
+
+function addShadowOnHeaderScroll() {
+  const header = document.querySelector('.header');
+  window.addEventListener('scroll',()=>{
+    if(window.scrollY >= 10) {
+      header.classList.add('header--box-shadow');
+    } else {
+      header.classList.remove('header--box-shadow');
+    }
+  })
+}
+addShadowOnHeaderScroll();
+
+import { magicMouse } from 'magicmouse.js';
+
+
+const options = {
+	"cursorOuter": "circle-basic",
+	"hoverEffect": "circle-move",
+	"hoverItemMove": true,
+	"defaultCursor": true,
+	"outerWidth": 16,
+	"outerHeight": 16
+  };
+  magicMouse(options);
+
+
+function targetsForMagicMouse(elems) {
+  elems.classList.add('magic-hover','magic-hover__square');
+}
+
+document.querySelectorAll('a, button, .reductor-types__card, .header__search').forEach(targetsForMagicMouse);
+
+const customCursor = document.querySelector('#magicMouseCursor');
+
+function toggleFadedClassOnCursor(elems) {
+  elems.addEventListener('focusin',()=>{
+    customCursor.classList.add('faded');
+  })
+  elems.addEventListener('focusout',()=>{
+    customCursor.classList.remove('faded');
+  })
+}
+
+document.querySelectorAll('input').forEach(toggleFadedClassOnCursor);
+
+function toggleClassOnCursor(elems) {
+  elems.addEventListener('mouseover',()=>{
+    customCursor.classList.add('hovered');
+  })
+  elems.addEventListener('mouseout',()=>{
+    customCursor.classList.remove('hovered');
+    customCursor.classList.remove('clicked');
+  })
+  elems.addEventListener('click',()=>{
+    customCursor.classList.add('clicked');
+  })
+}
+
+document.querySelectorAll('.magic-hover').forEach(toggleClassOnCursor);
